8f4cf60e7f56d6686389f0a15f8bba9bb34e1464
ramesh banakar <ramesh.s.b@campusuvce.in>
2025-11-25 05:37:30 +0530

    adding the two varibale approach

--- fibonaciSeries.java
+++ fibonaciSeries.java
@@ -1,7 +1,31 @@
 public class fibonaciSeries {
+//    fibonaci using the two varibales
+    public static void fibUsingTwoVaribales(int n){
+        if(n==0){
+            System.out.println(0);
+            return;
+        };
+        if(n==1){
+            System.out.println(1);
+            return;
+        };
+        int a=0;
+        int b=1;
+        int c=a+b;
+        System.out.print(a+" "+b+" ");
+        while(c<=n){
+            System.out.print(c+" ");
+            a=b;
+            b=c;
+            c=a+b;
+        }
+
+    }
 
     public static void main(String [] args){
-        System.out.println("Hello world");
+         int n=5;
+        fibUsingTwoVaribales(n);
+
     }
 
 }
