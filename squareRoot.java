be00514817fa796d685faf4d1fb6c8f1c94d01fd
ramesh banakar <ramesh.s.b@campusuvce.in>
2026-02-01 15:06:39 +0530

    first occurance of the number in the arr

--- squareRoot.java
+++ squareRoot.java
@@ -16,7 +16,31 @@ public class squareRoot {
         }
         return ans;
     }
+    public static int firstOccurance(int [] arr,int target){
+        if(arr.length==1 && arr[0]==target) return 0;
+        int l=0;
+        int h=arr.length-1;
+        int ans=0;
+        while(l<=h){
+            int mid=l+(h-l)/2;
+            if(arr[mid]<target){
+                l=mid+1;
+
+            }else if(arr[mid]>target){
+                h=mid+1;
+            }else{
+                ans=mid;
+                h=mid-1;
+            }
+        }
+        return ans;
+
+    }
     public static void main(String [] args){
-        System.out.println(findSquareroot(23));
+        System.out.println("Square Root of the given number:"+findSquareroot(23));
+
+        int [] arr={1,2,2,2,3,3,3,3,3,5,5,5,5,7,8,8,8,8,8};
+        int index=firstOccurance(arr,2);
+        System.out.println("First occurance of the arr: "+index);
     }
 }
